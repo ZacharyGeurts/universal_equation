477ed1bacaea8afa3be4b04be13da257510a37aa
Zachary Robert Geurts <gzac5314@gmail.com>
2025-09-17 09:21:32 -0400

    merge font

--- include/types.hpp
+++ include/types.hpp
@@ -2,6 +2,10 @@
 #define TYPES_HPP
 
 #include "glm/glm.hpp"
+#include <SDL3/SDL.h>
+#include <string>
+#include <unordered_map>
+#include <cstdint>
 
 struct DimensionData {
     int dimension;
@@ -22,6 +26,31 @@ struct PushConstants {
     float cycleProgress;
     float darkMatter;
     float darkEnergy;
+};
+
+struct Glyph {
+    SDL_Texture* texture;
+    int width;
+    int height;
+    int advance;
+    int offset_x;
+    int offset_y;
+};
+
+class Font {
+public:
+    Font(SDL_Renderer* renderer, int char_width, int char_height);
+    ~Font();
+    void render_text(SDL_Renderer* renderer, const std::string& text, int x, int y, SDL_Color color);
+    void measure_text(const std::string& text, int& width, int& height) const;
+
+private:
+    std::unordered_map<char, Glyph> glyphs_;
+    SDL_Renderer* renderer_;
+    int char_width_;
+    int char_height_;
+    bool load_font();
+    void free_glyphs();
 };
 
 #endif // TYPES_HPP
