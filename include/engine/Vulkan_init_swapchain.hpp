25a213c7ccc3ff38e04219c2bcf7263c786cd387
Zachary Robert Geurts <gzac5314@gmail.com>
2025-10-10 00:06:16 -0400

    Delete include/engine/Vulkan_init_swapchain.hpp

--- include/engine/Vulkan_init_swapchain.hpp
+++
@@ -1,8 +0,0 @@
-// AMOURANTH RTX Engine, October 2025 - Vulkan swapchain initialization.
-// Manages swapchain creation and resizing.
-// Dependencies: Vulkan 1.3+, C++20 standard library.
-// Zachary Geurts 2025
-
-#pragma once
-
-// Empty header; all definitions moved to Vulkan_init.hpp to avoid redefinition
